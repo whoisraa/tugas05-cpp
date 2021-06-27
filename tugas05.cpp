151341f84b0bee07ec305dc76f5a7cda79dd8e17
whoisraa <rairaiatlantica@gmail.com>
2021-06-27 22:42:50 +0700

    tugas 5 cpp

---
+++ tugas05.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main(){
+	int x, y, z;
+	cout<<"Inputkan Nilai Yang Ingin Dipangkatkan : ";
+	cin>>x;
+	cout<<"Inputkan Nilai Pangkat : ";
+	cin>>y;
+	z = pow(x,y);
+	cout<<"Hasil Per Pangkatan : "<<z;
+}
